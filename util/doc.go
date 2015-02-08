f265cd282c1f6165be37b0a37be1ab32861c94d0
Sören Tempel <soeren+git@soeren-tempel.net>
2015-02-08 18:47:20 +0100

    Fix package names

--- util/doc.go
+++ util/doc.go
@@ -11,7 +11,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-package feedparser
+package util
 
 /*
 Package util implements common utilities for implementing a FeedFunc.
